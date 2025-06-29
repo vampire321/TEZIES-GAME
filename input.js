8899017b5c01f2ce5a2b4ac6c5b855574ebb2354
Mithun H <mh1295551@gmail.com>
2025-06-29 13:13:53 +0530

    feat: add power-up system

--- input.js
+++ input.js
@@ -4,3 +4,9 @@
 const updateREADMEwithgameplayguide = () => {
   console.log('docs: update README with gameplay guide');
 };
+
+// feat: add power-up system
+// Commit #12 | 2026-07-25 08:24:22
+const addpowerupsystem = () => {
+  console.log('feat: add power-up system');
+};
