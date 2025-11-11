65a215a8164e8e40a798e2058620cd9be85ced12
vampire321 <mh1295551@gmail.com>
2025-11-11 14:27:18 +0530

    code for generating random number

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 import Die from "/Die.jsx"
+function generateAllNewDice(){
+/*const randomNumber = Math.floor(Math.random() * 6)+1;
+return randomNumber;*/
+// Create a new array (empty)
+        // Loop 10 times
+            // Generate a random number between 1-6
+            // Push that number to the array
+        // return the array
+  /*const newDice =[]
+  for (let i=0;i<10;i++){
+    const rand = Math.ceil(Math.random()*6)
+    newDice.push(rand)
+  }
+  return newDice
+      */
+    return new Array(10)
+      .fill(0)
+      .map(() => Math.ceil(Math.random() * 6))
+}
+console.log(generateAllNewDice())
 export default function App() {
-  return <main>
+  return (
+    <main>
     <div className="dice-container">
       <Die value={1}/>
       <Die value={2}/>
@@ -14,4 +35,5 @@ export default function App() {
       <Die value={10}/>
     </div>
   </main>
+  )
 }
