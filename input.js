0a8d45ba4a1e0122e95a41c2a07e55442efed3ba
Mithun H <mh1295551@gmail.com>
2026-07-06 09:14:29 +0530

    docs: update README with gameplay guide

---
+++ input.js
@@ -0,0 +1,6 @@
+
+// docs: update README with gameplay guide
+// Commit #17 | 2026-07-24 22:56:24
+const updateREADMEwithgameplayguide = () => {
+  console.log('docs: update README with gameplay guide');
+};
